160b200fb873fb1b423b4af6f6c562179e416417
tlevans3543 <42845995+tlevans3543@users.noreply.github.com>
2018-11-15 11:08:43 -0500

    Change: Reformatted files: Main.java, ProjectStyle.css, DbHandler.java to reflect google java style. Renamed "dbHandler.java" to "DbHandler.java".

--- src/GuiProject/dbHandler.java
+++ src/GuiProject/dbHandler.java
@@ -8,76 +8,73 @@ import java.sql.*;
  * Time: 5:54 PM
  * Description:
  */
-class dbHandler {
+class DbHandler {
 
-    private static final String DRIVER = "org.apache.derby.jdbc.EmbeddedDriver";
-    private static final String dbURL = "jdbc:derby:OperatorData";
+  private static final String DRIVER = "org.apache.derby.jdbc.EmbeddedDriver";
+  private static final String dbURL = "jdbc:derby:OperatorData";
 
-    private static Connection conn = null;
-    private static Statement stmt = null;
-    private static ResultSet rset = null;
+  private static Connection conn = null;
+  private static Statement stmt = null;
+  private static ResultSet rset = null;
 
-    /**
-     *
-     * @return
-     * @throws Exception
-     */
-    static Connection createConnection() throws Exception {
-        Class.forName(DRIVER);
-        conn = DriverManager.getConnection(dbURL);
-        return conn;
+  /**
+   * @return
+   * @throws Exception
+   */
+  static Connection createConnection() throws Exception {
+    Class.forName(DRIVER);
+    conn = DriverManager.getConnection(dbURL);
+    return conn;
+  }
+
+  /**
+   * @return
+   * @throws Exception
+   */
+  static String[] getAtkOperator() throws Exception {
+
+    String[] atkArr = new String[2];
+
+    try {
+      conn = createConnection();
+      stmt = conn.createStatement();
+      rset =
+          stmt.executeQuery(
+              "SELECT * FROM ATTACKERS ORDER BY RANDOM() OFFSET 0 ROWS " + "FETCH NEXT 1 ROW ONLY");
+      while (rset.next()) {
+        String name = rset.getString("NAME");
+        String image = rset.getString("IMAGE");
+        atkArr[0] = name;
+        atkArr[1] = image;
+      }
+    } catch (SQLException e) {
+      e.printStackTrace();
     }
 
-    /**
-     *
-     * @throws Exception
-     */
-    static String[] getAtkOperator() throws Exception {
+    return atkArr;
+  }
 
-        String[] atkArr = new String[2];
+  /** @throws Exception */
+  static String[] getDefOperator() throws Exception {
 
-        try{
-            conn = createConnection();
-            stmt = conn.createStatement();
-            rset = stmt.executeQuery("SELECT * FROM ATTACKERS ORDER BY RANDOM() OFFSET 0 ROWS " +
-                                    "FETCH NEXT 1 ROW ONLY");
-            while (rset.next()) {
-                String name = rset.getString("NAME");
-                String image = rset.getString("IMAGE");
-                atkArr[0] = name;
-                atkArr[1] = image;
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+    String[] defArr = new String[2];
 
-        return atkArr;
+    try {
+      conn = createConnection();
+      stmt = conn.createStatement();
+      rset =
+          stmt.executeQuery(
+              "SELECT * FROM DEFENDERS ORDER BY RANDOM() OFFSET 0 ROWS " + "FETCH NEXT 1 ROW ONLY");
+      while (rset.next()) {
+        String name = rset.getString("NAME");
+        String image = rset.getString("IMAGE");
+        defArr[0] = name;
+        defArr[1] = image;
+      }
+    } catch (SQLException e) {
+      e.printStackTrace();
     }
 
-    /**
-     *
-     * @throws Exception
-     */
-    static String[] getDefOperator() throws Exception {
-
-        String[] defArr = new String[2];
-
-        try{
-            conn = createConnection();
-            stmt = conn.createStatement();
-            rset = stmt.executeQuery("SELECT * FROM DEFENDERS ORDER BY RANDOM() OFFSET 0 ROWS " +
-                                    "FETCH NEXT 1 ROW ONLY");
-            while (rset.next()) {
-                String name = rset.getString("NAME");
-                String image = rset.getString("IMAGE");
-                defArr[0] = name;
-                defArr[1] = image;
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
-        return defArr;
-    }
-
+    return defArr;
+  }
 }
